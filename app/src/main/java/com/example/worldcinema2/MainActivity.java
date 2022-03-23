133badeb247122e5dfe469da3639a14e5316c0a3
Wwwwwwwwww28 <kaimasida@mail.ru>
2022-03-23 17:04:16 +0600

    Initial commit

---
+++ app/src/main/java/com/example/worldcinema2/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.worldcinema2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getSupportActionBar().hide();
+        setContentView(R.layout.activity_main);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+                Intent i = new Intent( MainActivity.this, SignInScreen.class);
+                startActivity(i);
+
+                finish();
+            }
+        }, 3000);
+
+    }
+}
